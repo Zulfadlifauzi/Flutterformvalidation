677be42e6d68cca995aeaa2729401d60c7db42f2
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-11-08 11:08:26 +0800

    add fetch data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:formvalid/screens/data.dart';
 import 'package:formvalid/screens/login.dart';
 
 void main() {
@@ -6,7 +7,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -24,8 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: HomeData(),
     );
   }
 }
-
